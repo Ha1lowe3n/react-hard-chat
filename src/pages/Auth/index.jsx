6227396ceb23c65bab1ba294ffef17d02bc18237
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-01-28 21:09:00 +0800

    end layout Log In

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,15 +1,77 @@
 import React from 'react';
 
+import { Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import './Auth.scss';
+
 import { Block, Button } from '../../components';
 
 function Auth() {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
     <section className="auth">
-      <Block>
-        <Button type="primary" size="large">
-          This is Button
-        </Button>
-      </Block>
+      <div className="auth__content">
+        <div className="auth__top">
+          <h2>Войти в аккаунт</h2>
+          <p>Пожалуйста, войдите в свой аккаунт</p>
+        </div>
+
+        <Block>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}>
+            <Form.Item
+              name="username"
+              hasFeedback
+              validateStatus="success"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Username!',
+                },
+              ]}>
+              <Input
+                size="large"
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Password!',
+                },
+              ]}>
+              <Input
+                size="large"
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="button--large">
+                Войти в аккаунт
+              </Button>
+            </Form.Item>
+
+            <a className="auth__register-link" href="#">
+              Зарегистрироваться
+            </a>
+          </Form>
+        </Block>
+      </div>
     </section>
   );
 }
